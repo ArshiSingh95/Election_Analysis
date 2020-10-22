ca7b89df325c49e16ef3a91285ac70d69706fb07
Arsdip <sarsdip@gmail.com>
2020-10-21 19:37:18 -0700

    update

--- Py.poll_final.py
+++ Py.poll_final.py
@@ -6,9 +6,47 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis, election_analysis.txt")
 
+#1. Intialize total vote customer
+total_votes = 0
+#Print Canddidate Name
+candidate_options= []
+#1 Declare the empty dictionary
+candidate_votes = {}
+
+
 #open the election results and read the file
 with open(file_to_load) as (election_data):
     file_reader = csv.reader(election_data)
     #Print headers Row
     headers = next(file_reader)
     print(headers)
+    for row in file_reader:
+        #2 add the total vote count
+        total_votes+= 1
+
+#Print total votes
+  
+
+        candidate_name = row[2]
+    #Add candidate name to candidate last and make sure it is not double counted
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+            #2 begin tracking candidate votes
+            candidate_votes[candidate_name]= 0
+        else:
+            candidate_votes[candidate_name] += 1 
+    #Print candidate list
+    
+    print(candidate_options)
+    print(candidate_votes)
+    #1 Itierate throuhgh the candidate list
+    for candidate_name in candidate_votes:
+    #Get vote count of Candidate 
+        votes = candidate_votes[candidate_name]
+    # calcluate percentage of votes
+        vote_percentage= float(votes)/float(total_votes) * 100
+    #Print Candidate name and Percentage of votes
+        candidate_results=(
+            f"{candidate_name} : recieved {vote_percentage} % of the votes in this election.")
+        print(candidate_results)
